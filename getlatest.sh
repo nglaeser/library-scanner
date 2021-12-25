89d7a9b9ccd0b88ed653dcfc2cfaf9297096a506
nglaeser <nglaeser@users.noreply.github.com>
2021-12-25 18:04:13 +0100

    bugfixes in fetch script

--- getlatest.sh
+++ getlatest.sh
@@ -3,16 +3,22 @@
 # The cronjob assumes this is run in ~, but the script is written to work from any dir
 
 repo="https://github.com/nglaeser/library-scanner"
-url=$(curl -Ls -o /dev/null -w %{url_effective} $repo/releases/latest)
+# repo must be public for this line to work
+url=$(curl -Ls -o /dev/null -w %{url_effective} $repo/releases/latest/)
 tag=$(basename $url)
+# alternative (uses tags):
+# git fetch --tags
+# tag=$(git describe --tags `git rev-list --tags --max-count=1`)
 
 cd ~/library-scanner
 # pull newest release
 git pull origin $tag
+# or the following??
+# git checkout $tag
+chmod +x getlatest.sh
 # return to previous directory
 cd -
-# cp ~/library-scanner/getlatest.sh .
-# chmod +x getlatest.sh
+
 # restart scanner script
 pkill python
 python main.py
