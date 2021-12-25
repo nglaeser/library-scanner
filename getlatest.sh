24b547934716be011bea0a8e657b2f13160f3fd4
nglaeser <nglaeser@users.noreply.github.com>
2021-12-25 12:16:52 +0100

    update cronjob script

--- getlatest.sh
+++ getlatest.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
+# Assumes the repo has already been cloned in ~
+# The cronjob assumes this is run in ~, but the script is written to work from any dir
 
-# test repo
-# repo="https://github.com/nglaeser/graph_cyclone"
 repo="https://github.com/nglaeser/library-scanner"
 url=$(curl -Ls -o /dev/null -w %{url_effective} $repo/releases/latest)
 tag=$(basename $url)
-git pull origin $tag
 
-# restart script
-pkill Python
+cd ~/library-scanner
+# pull newest release
+git pull origin $tag
+# return to previous directory
+cd -
+# cp ~/library-scanner/getlatest.sh .
+# chmod +x getlatest.sh
+# restart scanner script
+pkill python
 python main.py
